82634e27071b960cd08557821a4367f0986a2825
Jason9789 <lifebook0809@gmail.com>
2023-10-03 16:17:03 +0900

    add: Async Image

---
+++ SwiftUIAsynchImageApp/SwiftUIAsynchImageApp/SwiftUIAsynchImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsynchImageAppApp.swift
+//  SwiftUIAsynchImageApp
+//
+//  Created by 전판근 on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsynchImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
